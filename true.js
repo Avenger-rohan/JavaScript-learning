564e6bf54f326351446885c989d38137943a3b96
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-03-23 07:16:32 +0530

    Add files via upload

---
+++ true.js
@@ -0,0 +1,15 @@
+function test(wasThatTrue){
+    if(wasThatTrue){
+        return "yes this is true"
+    }
+return "this is false";
+}
+
+console.log (test(false));
+
+
+
+// function mySeven(num){
+    //     console.log(num-2);
+    // }
+    //   mySeven(5)
