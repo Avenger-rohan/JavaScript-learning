19e6444302732b0378a9c765adf65b6a508c246e
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-08 22:39:28 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,13 @@
+
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+
+
+
+console.log(person.name);  // Output: John
+console.log(person.age);   // Output: 30
+console.log(person.city);
