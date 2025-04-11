d9a35776905449997bdb6b68dca8b953e18d6456
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-11 21:29:18 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,9 @@
+const accountId="Bca @kuk@khalsa"
+var myName;
+var phone=1452365
+let accountEmail="Rohan@google.com"
+var accountPassword=12222
+account_City="bengluru"
+
+
+console.table([accountEmail,accountId,accountPassword,account_City,phone,myName]);
