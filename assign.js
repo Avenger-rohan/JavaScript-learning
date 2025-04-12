414bce0edf257a0df9ec0c8d1c55665217b74aef
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-13 05:08:47 +0530

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,18 @@
+// let myChannel="mr killer"
+// let anotherName=myChannel
+// anotherName="Test waala"
+// console.log(myChannel);
+// console.log(anotherName)
+
+
+
+let userOne={
+    email:"rohan@google.com",
+    upi:"user@ypik",
+}
+
+let userTwo=userOne
+userTwo.email="hitesh@google.com"
+console.log(userOne);
+console.log(userTwo);
+
