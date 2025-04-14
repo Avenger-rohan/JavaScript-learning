77b2ffd1474eb66cdc0aa641e9427a97ac1b1739
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-14 12:43:05 +0530

    Add files via upload

---
+++ slice.js
@@ -0,0 +1,12 @@
+const myArray=[1,2,3,4,5]
+
+const myArray1=myArray.slice(1,3)
+console.log(myArray1);
+console.log("a",myArray);
+
+console.log("b",myArray);
+
+const myArray2=myArray.splice(1,3)
+console.log("c",myArray);
+
+console.log(myArray2);
