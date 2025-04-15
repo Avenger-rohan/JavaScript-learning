86c5bef8acb71d8629fd0dcbb65ba9ca3f77465a
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-15 20:24:41 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,19 @@
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myDate=new Date(2003,0,14)
+// console.log(myDate.toDateString());
+
+let myDate=new Date("01-12-2023")
+// console.log(myDate.toLocaleString());
+
+
+let myTimeStamp=D9ate.now()
+console.log(myTimeStamp)
+
+console.log(myDate.getTime());
+
+
