2caa2eddc13aea42668b68376f84455f6a334793
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-04-15 20:30:04 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,12 @@
+function check(num){
+    let i="this is i";
+    if(num){
+        let i="this is true"
+       console.log("represent ",i);
+       return;
+}
+    console.log("wow",i);
+  
+}
+     console.log(check(1));
+     
