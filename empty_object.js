5ea864041f448a89414668460a985f69281cd1ff
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-05-02 23:02:41 +0530

    Add files via upload

---
+++ empty_object.js
@@ -0,0 +1,30 @@
+// const userEmail=[]
+// if(userEmail){
+//     console.log("user got emai");
+    
+// }
+// else{
+//     console.log("user not got email");
+    
+// }
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+    
+// }
+
+
+
+// const obj={}
+// if(Object.keys(obj).length===0){
+//     console.log("öbject is empty");
+    
+// }
+
+
+
+const isDebitCard=false
+const isGooglePay=true
+if(isDebitCard||isGooglePay){
+    console.log("user can pay");
+    
+}
