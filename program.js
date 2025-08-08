cbea8e093e6eab4672d6a1b23de1ab267621e1ce
Rohan <121714421+Avenger-rohan@users.noreply.github.com>
2025-08-08 22:17:05 +0530

    Add files via upload

---
+++ program.js
@@ -0,0 +1,13 @@
+// let arr=[4,6,8,3,15,21,2,1,7]
+// let acess=arr.sort()
+// console.log(acess);
+
+
+
+
+
+let str="i love to eat pizza everyday"
+let s=str.split(" ")
+let output=s.filter((val)=>{
+    return val.length>3})
+console.log(output);
